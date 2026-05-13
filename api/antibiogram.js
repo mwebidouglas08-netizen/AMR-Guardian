715264b30af93f3d9cdf5cfcb0c7ea0e9696e6cb
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-05-13 10:51:22 +0300

    Create antibiogram.js

---
+++ api/antibiogram.js
@@ -0,0 +1,7 @@
+const { ANTIBIOGRAM, cors } = require('./_data');
+
+module.exports = async (req, res) => {
+  cors(res);
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  res.status(200).json(ANTIBIOGRAM);
+};
