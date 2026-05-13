0472327a55673780bf1bc2f3e362129d443fd62b
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-05-13 14:07:24 +0300

    Create regional.js

---
+++ frontend/api/resistance/regional.js
@@ -0,0 +1,7 @@
+const { REGIONAL, cors } = require('../_data');
+
+module.exports = async (req, res) => {
+  cors(res);
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  res.status(200).json(REGIONAL);
+};
